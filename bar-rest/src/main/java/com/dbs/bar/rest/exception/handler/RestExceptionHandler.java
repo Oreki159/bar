4739183d702d25e9306028230c45f1d0081a2c45
Jorge Luis Alvarez Alvarez <jalvarea@everis.com>
2017-02-25 08:51:53 -0500

    Add logs in RestExceptionHandler

--- bar-rest/src/main/java/com/dbs/bar/rest/exception/handler/RestExceptionHandler.java
+++ bar-rest/src/main/java/com/dbs/bar/rest/exception/handler/RestExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.dbs.bar.rest.exception.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -8,16 +10,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class RestExceptionHandler {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(RestExceptionHandler.class);
+	
 	@ExceptionHandler(Exception.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	public void processException() {
-
+	public void processException(Exception ex) {
+		LOGGER.error(ex.getMessage(), ex);
 	}
 
 	@ExceptionHandler(RuntimeException.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	public void processRuntimeException(RuntimeException ex) {
-
+		LOGGER.error(ex.getMessage(), ex);
 	}
 
 }
