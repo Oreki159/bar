5fda279da8fd7822a04994286189e42cdba92cb6
Jorge Luis Alvarez Alvarez <jalvarea@everis.com>
2017-02-24 23:30:13 -0500

    Add Exception Handler

---
+++ bar-rest/src/main/java/com/dbs/bar/rest/exception/handler/RestExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.dbs.bar.rest.exception.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public void processException() {
+
+	}
+
+	@ExceptionHandler(RuntimeException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public void processRuntimeException() {
+
+	}
+
+}
