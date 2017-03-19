eda638d53d09fd5048915e480ce98b7ba870a73e
Jorge Luis Alvarez Alvarez <jalvarea@everis.com>
2017-03-18 19:27:51 -0500

    Add uri publics

--- bar-security/src/main/java/com/dbs/bar/security/constant/SecurityConstant.java
+++ bar-security/src/main/java/com/dbs/bar/security/constant/SecurityConstant.java
@@ -2,7 +2,9 @@ package com.dbs.bar.security.constant;
 
 public final class SecurityConstant {
 
-	public static final String URL_LOGIN = "/app/security/api/login";
+	public static final String	URL_LOGIN			= "/app/security/api/login";
+
+	public static final String	URL_CUSTOMER_SAVE	= "/app/customers";
 
 	private SecurityConstant() {}
 
